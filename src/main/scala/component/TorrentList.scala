68a468c21231ee1494031bac91eb4a4d2cbf2761
Vitaly Lavrov <lavrovvv@gmail.com>
2020-11-14 23:32:51 +0100

    Add space between search result items

--- src/main/scala/component/TorrentList.scala
+++ src/main/scala/component/TorrentList.scala
@@ -2,6 +2,7 @@ package component
 
 import com.github.lavrov.bittorrent.InfoHash
 import component.material_ui.core.CardActionArea
+import component.material_ui.styles.makeStyles
 import slinky.core.FunctionalComponent
 import slinky.core.annotations.react
 import slinky.web.html._
@@ -9,19 +10,31 @@ import squants.experimental.formatter.Formatters.InformationMetricFormatter
 import typings.materialUiCore.components.{Card, CardContent, Typography, Chip}
 import squants.information.Information
 
+import scala.scalajs.js.Dynamic.{literal => obj}
+
 @react
 object TorrentList {
 
   case class Props(title: String, items: List[(InfoHash, String, Information, List[String])])
 
+  val useStyles = makeStyles { theme =>
+    obj(
+      item = obj(
+        marginBottom = theme.spacing(1)
+      )
+    )
+  }
+
   val component = FunctionalComponent[Props] { props =>
+
+    val classes = useStyles()
 
     def handleClick(infoHash: InfoHash) = () => Navigate(Routes.torrent(infoHash))
 
     for {
       ((infoHash, title, size, ext), index) <- props.items.zipWithIndex
     } yield {
-      Card(
+      Card.className(classes.item.toString)(
         CardActionArea()(
           key := s"torrent-list-item-$index",
           onClick := handleClick(infoHash),
