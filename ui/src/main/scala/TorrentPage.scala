ce35e6824b3bb1dbf72b91ecf071a4b406dde811
Vitaly Lavrov <vitaly.lavrov@getbux.com>
2021-03-28 14:10:49 +0200

    Show image content

--- ui/src/main/scala/TorrentPage.scala
+++ ui/src/main/scala/TorrentPage.scala
@@ -4,6 +4,8 @@ import com.github.lavrov.bittorrent.InfoHash
 import com.github.lavrov.bittorrent.app.protocol.{Command, Event}
 import com.raquo.domtypes.generic.codecs.StringAsIsCodec
 import com.raquo.laminar.api.L._
+import com.raquo.laminar.nodes.{ReactiveElement, ReactiveHtmlElement}
+
 import scala.util.chaining.scalaUtilChainingOps
 
 
@@ -15,14 +17,6 @@ object TorrentPage {
 
     def videoUrl(index: Int) =
       s"https://bittorrent-server.herokuapp.com/torrent/$infoHash/data/$index"
-
-    def isPlayable(src: String) = {
-      val ext = src.split('.').lastOption
-      ext match {
-        case Some("mp4" | "avi" | "webm" | "mkv") => true
-        case _ => false
-      }
-    }
 
     val content =
       events
@@ -52,15 +46,10 @@ object TorrentPage {
                           href := videoUrl(index),
                           "Download"
                         ),
-                        isPlayable(file.path.last).pipe {
-                          case true =>
-                            button(cls := "button is-small",
-                              onClick.mapTo(ActiveFile(file.path.last, videoUrl(index))).map(Some(_)) --> showModalVar,
-                              "Watch"
-                            )
-                          case _ =>
-                            div()
-                        }
+                        button(cls := "button is-small",
+                          onClick.mapTo(ActiveFile(file.path.last, videoUrl(index))).map(Some(_)) --> showModalVar,
+                          "Open"
+                        )
                       )
                     )
                   )
@@ -73,7 +62,7 @@ object TorrentPage {
       onMountCallback(_ => send.onNext(Command.GetTorrent(infoHash))),
       children <-- content,
       child <-- showModalVar.signal.map {
-        case Some(file) => videoModal(file, showModalVar.toObserver.contramap(_ => None))
+        case Some(file) => openModal(file, showModalVar.toObserver.contramap(_ => None))
         case None => div()
       }
     )
@@ -83,31 +72,80 @@ object TorrentPage {
 
   private val controls = customProp("controls", StringAsIsCodec)
 
-  private def videoModal(activeFile: ActiveFile, close: Observer[Any]) = {
-    div(cls:="modal is-active",
-      div(cls:="modal-background",
-        onClick --> close
-      ),
-      div(cls:="modal-content",
-        div(cls := "card",
-          div(cls := "card-image",
-            video(cls := "is-4by3",
-              width := "100%",
-              controls := "true",
-              source(
-                src := activeFile.src
-              )
-            ),
-            div(cls:="card-content",
-              div(cls:="media",
-                div(cls:="media-content",
-                  h4(cls:="title is-4", activeFile.name)
-                )
-              )
+  private def openModal(activeFile: ActiveFile, close: Observer[Any]) = {
+    val ext = activeFile.name.split('.').lastOption
+    ext match {
+      case Some("mp4" | "avi" | "webm" | "mkv") => videoModal(activeFile, close)
+      case Some("jpeg" | "jpg" | "png" | "gif") => imageModal(activeFile, close)
+      case _ => genericModal(activeFile, close)
+    }
+  }
+
+  private def genericModal(activeFile: ActiveFile, close: Observer[Any]) = {
+    modal(
+      div(cls := "card",
+        div(cls:="card-content",
+          div(cls:="media",
+            div(cls:="media-content",
+              h4(cls:="title is-4", activeFile.name)
             )
           )
         )
       ),
+      close
+    )
+  }
+
+  private def videoModal(activeFile: ActiveFile, close: Observer[Any]) = {
+    modal(
+      div(cls := "card",
+        div(cls := "card-image",
+          video(cls := "is-4by3",
+            width := "100%",
+            controls := "true",
+            source(
+              src := activeFile.src
+            )
+          )
+        ),
+        div(cls:="card-content",
+          div(cls:="media",
+            div(cls:="media-content",
+              h4(cls:="title is-4", activeFile.name)
+            )
+          )
+        )
+      ),
+      close
+    )
+  }
+
+  private def imageModal(activeFile: ActiveFile, close: Observer[Any]) = {
+    modal(
+      div(cls := "card",
+        div(cls := "card-image",
+          figure(cls := "image is-4by3",
+            img(src := activeFile.src)
+          ),
+        ),
+        div(cls:="card-content",
+          div(cls:="media",
+            div(cls:="media-content",
+              h4(cls:="title is-4", activeFile.name)
+            )
+          )
+        )
+      ),
+      close
+    )
+  }
+
+  private def modal(content: ReactiveHtmlElement[org.scalajs.dom.html.Div], close: Observer[Any]) = {
+    div(cls:="modal is-active",
+      div(cls:="modal-background",
+        onClick --> close
+      ),
+      div(cls:="modal-content", content),
       button(cls:="modal-close is-large", aria.label:="close",
         onClick --> close
       )
