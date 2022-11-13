b38464cd21fdf0186abe67787acde9940ac0999b
Vitaly Lavrov <vitaly.lavrov@coralogix.com>
2022-11-13 15:57:00 +0100

    Best unit formatter fix

--- app/src/main/scala/util/InformationFormatter.scala
+++ app/src/main/scala/util/InformationFormatter.scala
@@ -4,7 +4,7 @@ import squants.information.{Bytes, Gigabytes, Information, Kilobytes, Megabytes}
 
 object InformationFormatter {
 
-  private val units = List(Bytes, Kilobytes, Megabytes, Gigabytes)
+  private val units = List(Gigabytes, Megabytes, Kilobytes, Bytes)
   
   def inBestUnit(bytes: Information): Information =
     units.find(unit => bytes.to(unit) >= 1).map(bytes.in).getOrElse(bytes)
